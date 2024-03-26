cf4b5f663774e0133acaa481fba5fa1e1b5d25f5
Bilal-Shahzad <b.shahzad01@gmail.com>
2024-03-25 21:37:46 -0500

    test

--- notes/stacks/StorageStack.ts
+++ notes/stacks/StorageStack.ts
@@ -2,8 +2,17 @@ import { Bucket, StackContext, Table } from "sst/constructs";
 
 export function StorageStack({ stack }: StackContext) {
   // Create the DynamoDB table
-  const bucket = new Bucket(stack, "Uploads");
-  const table = new Table(stack, "Notes", {
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
+    const table = new Table(stack, "Notes", {
     fields: {
       userId: "string",
       noteId: "string",
