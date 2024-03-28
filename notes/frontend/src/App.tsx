e29757fa309eaada1e3f355ed5a2a3b68aaafa85
Bilal-Shahzad <b.shahzad01@gmail.com>
2024-03-27 22:10:17 -0500

    adding links to the navbar and 404 handlers

--- notes/frontend/src/App.tsx
+++ notes/frontend/src/App.tsx
@@ -4,15 +4,21 @@ import 'bootstrap/dist/css/App.css';
 import Routes from "./Routes.tsx";
 
 function App() {
-    return (
-      <div className="App container py-3">
-        <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
-          <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar>
-        <Routes />
-      </div>
-    );
-  }
+  return (
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
+        <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Nav.Link href="/signup">Signup</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Routes />
+    </div>
+  );
+}
 
 export default App;
