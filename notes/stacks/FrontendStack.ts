689a675ffaae2022998651c0d1fe04c931df3dae
Bilal-Shahzad <b.shahzad01@gmail.com>
2024-05-06 23:28:31 -0500

    setting up custom domains

--- notes/stacks/FrontendStack.ts
+++ notes/stacks/FrontendStack.ts
@@ -10,6 +10,13 @@ export function FrontendStack({ stack, app }: StackContext) {
 
   // Define our React app
   const site = new StaticSite(stack, "ReactSite", {
+    customDomain:
+  app.stage === "prod"
+    ? {
+        domainName: "<YOUR DOMAIN>",
+        domainAlias: "www.<YOUR DOMAIN>",
+      }
+    : undefined,
     path: "packages/frontend",
     buildCommand: "pnpm run build",
     buildOutput: "dist",
@@ -19,14 +26,16 @@ export function FrontendStack({ stack, app }: StackContext) {
       VITE_REGION: app.region,
       VITE_BUCKET: bucket.bucketName,
       VITE_USER_POOL_ID: auth.userPoolId,
-      VITE_API_URL: api.customDomainUrl || api.url,
-      VITE_USER_POOL_CLIENT_ID: auth.userPoolClientId,
+      VITE_API_URL: api.customDomainUrl || api.url,     
+       VITE_USER_POOL_CLIENT_ID: auth.userPoolClientId,
       VITE_IDENTITY_POOL_ID: auth.cognitoIdentityPoolId || "",
     },
   });
 
+  
   // Show the url in the output
   stack.addOutputs({
     SiteUrl: site.customDomainUrl || site.url,
   });
+  
 }
